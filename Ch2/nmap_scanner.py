c78947108a69d7c841994b48738c3cecf80f09ba
Hasnat Md. Abdullah <hasnatabdullah79@gmail.com>
2020-06-17 15:05:25 +0600

    uses nmap to see if given ports are open, close or filtered. ps. nmap needs to be installed in the os

---
+++ Ch2/nmap_scanner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import nmap
+import argparse
+
+def nmap_scan(tgt_host,tgt_ports):
+    nm_scan=nmap.PortScanner()
+    for tgt_port in tgt_ports:
+        nm_scan.scan(tgt_host,tgt_port)
+        state = nm_scan[tgt_host]['tcp'][int(tgt_port)]['state']
+        print(f'[*] {tgt_host} tcp/{tgt_port} {state}')
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser(usage='npam_scan.py TARGET_HOST -p TARGET_PORTS')
+    parser.add_argument('host',type=str,metavar='TARGET_HOST',help="specify target host's IP nubmer")
+    parser.add_argument('-p',type=str,metavar='TARGET_PORTS',help="specify target port[s] separated by comma")
+    args=parser.parse_args()
+
+    args.ports=str(args.p).split(',')
+    nmap_scan(args.host,args.ports)
